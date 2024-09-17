97bdbc10cc62d50a267b37bb9399cf55057bc594
SeeingTimes <31857374+jermmy19998@users.noreply.github.com>
2024-09-17 10:59:31 +0800

    Update heatmap.sh

--- shells/heatmap.sh
+++ shells/heatmap.sh
@@ -1,46 +1,13 @@
-python ./push_code/heatmap/make_heatmap.py \
+python ./source/make_heatmap.py \
     --base_dir . \
-    --test_csv ./push_code/out_csv/OC_202.csv \
-    --p8_feature_dir ./202_40x_ViT_p8_448_0.5 \
-    --p16_feature_dir ./202_40x_ViT_p16_448_0.5 \
-    --abmil_p8_weight ./ckpt/weighted/abmil8_epoch_7_loss_0.2347_bacc_0.9583.pth \
-    --abmil_p16_weight ./ckpt/weighted/abmil16_epoch_7_loss_0.2939_bacc_0.9375.pth \
-    --dsmil_p8_weight ./ckpt/dsmil8_epoch_15_loss_0.3595_bacc_0.8958.pth \
-    --dsmil_p16_weight ./ckpt/dsmil16_epoch_8_loss_0.6246_bacc_0.9583.pth \
+    --test_csv ./out_csv/your.csv \
+    --p8_feature_dir ./ViT_p8_448_0.5 \
+    --p16_feature_dir ./ViT_p16_448_0.5 \
+    --abmil_p8_weight ./ckpt/weighted/abmil8.pth \
+    --abmil_p16_weight ./ckpt/weighted/abmil16.pth \
+    --dsmil_p8_weight ./ckpt/dsmil8.pth \
+    --dsmil_p16_weight ./ckpt/dsmil16.pth \
     --num_worker 64 \
-    --remove_label \
-    --heatmap_save_dir ./push_code/heatmap/out_heatmap_202 \
-    --patch_dir ./202_40x_448_0.5 \
-
-
-
-python ./push_code/heatmap/make_heatmap.py \
-    --base_dir . \
-    --test_csv ./push_code/out_csv/OC_lc.csv \
-    --p8_feature_dir ./lc_40x_ViT_p8_448_0.5 \
-    --p16_feature_dir ./lc_40x_ViT_p16_448_0.5 \
-    --abmil_p8_weight ./ckpt/weighted/abmil8_epoch_7_loss_0.2347_bacc_0.9583.pth \
-    --abmil_p16_weight ./ckpt/weighted/abmil16_epoch_7_loss_0.2939_bacc_0.9375.pth \
-    --dsmil_p8_weight ./ckpt/dsmil8_epoch_15_loss_0.3595_bacc_0.8958.pth \
-    --dsmil_p16_weight ./ckpt/dsmil16_epoch_8_loss_0.6246_bacc_0.9583.pth \
-    --num_worker 64 \
-    --remove_label \
-    --heatmap_save_dir ./push_code/heatmap/out_heatmap_lc \
-    --patch_dir ./lc_40x_448_0.5
-
-
-
-python ./push_code/heatmap/make_heatmap.py \
-    --base_dir . \
-    --test_csv ./push_code/out_csv/OC_sc.csv \
-    --p8_feature_dir ./sc_40x_ViT_p8_448_0.5 \
-    --p16_feature_dir ./sc_40x_ViT_p16_448_0.5 \
-    --abmil_p8_weight ./ckpt/weighted/abmil8_epoch_7_loss_0.2347_bacc_0.9583.pth \
-    --abmil_p16_weight ./ckpt/weighted/abmil16_epoch_7_loss_0.2939_bacc_0.9375.pth \
-    --dsmil_p8_weight ./ckpt/dsmil8_epoch_15_loss_0.3595_bacc_0.8958.pth \
-    --dsmil_p16_weight ./ckpt/dsmil16_epoch_8_loss_0.6246_bacc_0.9583.pth \
-    --num_worker 64 \
-    --remove_label \
-    --heatmap_save_dir ./push_code/heatmap/out_heatmap_sc \
-    --patch_dir ./sc_40x_448_0.5
+    --heatmap_save_dir ./out_heatmap \
+    --patch_dir ./40x_448_0.5 \
 
